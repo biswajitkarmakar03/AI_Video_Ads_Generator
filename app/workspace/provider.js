97ba4333355e409196575388f2ef83ec53060b75
biswajitkarmakar03 <biswajitkarmakar404@gmail.com>
2025-05-22 14:45:15 +0530

    Building Workspace Auth

---
+++ app/workspace/provider.js
@@ -0,0 +1,33 @@
+"use client";
+import { CreateNewUser } from '@/convex/users';
+import { api } from '@/convex/_generated/api';
+import { useUser } from '@clerk/nextjs';
+import { useMutation } from 'convex/react';
+import React, { useEffect } from 'react'
+
+function WorkspaceProvider({ children }) {
+
+    const newUserMutation = useMutation(api.users.CreateNewUser);
+    const {user} = useUser(); 
+
+    useEffect(() => {
+      user && CreateNewUser()
+    }, [user])
+    
+    const CreateNewUser = async() => {
+        const result = await newUserMutation({
+            name:user?.fullName,
+            email:user?.primaryEmailAddress?.emailAddress,
+            picture:user?.imageUrl
+        });
+        console.log(result);
+    }
+
+  return (
+    <div>{children}</div>
+  )
+}
+
+export default WorkspaceProvider
+
+
